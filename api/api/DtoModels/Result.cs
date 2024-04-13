cb924ed900cc2871113f7226cb97df6ab0fd7581
Eg0rS <Ehrgit@yandex.ru>
2024-04-13 14:28:43 +0300

    asdasd

--- api/api/DtoModels/Result.cs
+++ api/api/DtoModels/Result.cs
@@ -8,8 +8,20 @@ public class Result
     public string UserId { get; set; }
     [JsonPropertyName("guid")]
     public string Guid { get; set; }
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+    [JsonPropertyName("series")]
+    public string Series { get; set; }
+    [JsonPropertyName("number")]
+    public string Number { get; set; }
+    [JsonPropertyName("page_number")]
+    public int? PageNumber { get; set; }
+    [JsonPropertyName("confidence")]
+    public decimal Confidence { get; set; }
     [JsonPropertyName("file_id")]
     public string FileId { get; set; }
     [JsonPropertyName("data")]
     public Dictionary<string,string> Data { get; set; }
+    [JsonPropertyName("status")]
+    public int Status { get; set; }
 }
