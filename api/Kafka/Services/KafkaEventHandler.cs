9edf8b057b29dedd36c061c9041f6fe518059d6f
Eg0rS <Ehrgit@yandex.ru>
2024-04-14 03:15:42 +0300

    я пушу в мастер потому что забыл записать результат распознавания

--- api/Kafka/Services/KafkaEventHandler.cs
+++ api/Kafka/Services/KafkaEventHandler.cs
@@ -21,18 +21,19 @@ public class KafkaEventHandler
         var massageJson = JsonConvert.DeserializeObject<ResultMessage>(message);
         OnMessageReceived(massageJson);
         var queryObject = new QueryObject(
-            "INSERT INTO results (guid, type, series, number, confidence, page_number, file_id, data) VALUES (@guid, @type, @series, @number, @confidence, @page_number, @file_id, @data)",
-        new
-        {
-            guid = massageJson.Guid,
-            type = massageJson.Type,
-            series = massageJson.Series,
-            number = massageJson.Number,
-            confidence = massageJson.Confidence,
-            page_number = massageJson.PageNumber,
-            file_id = massageJson.FileId,
-            data = JsonConvert.SerializeObject(massageJson.Data)
-        });
+            "INSERT INTO resolutions (guid, type, series, number, confidence, page_number, file_id, data) VALUES (@guid, @type, @series, @number, @confidence, @page_number, @file_id, @data)",
+            new
+            {
+                guid = massageJson.Guid,
+                type = massageJson.Type,
+                series = massageJson.Series,
+                number = massageJson.Number,
+                confidence = massageJson.Confidence,
+                page_number = massageJson.PageNumber,
+                file_id = massageJson.FileId,
+                data = JsonConvert.SerializeObject(massageJson.Data)
+            });
+        await connection.Command(queryObject);
     }
 
     protected virtual void OnMessageReceived(ResultMessage e)
